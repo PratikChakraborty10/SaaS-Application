3e63ffcad6ec7ab992be87bf67508e6bfccd6eb2
Pratik Chakraborty <chakrabortypratik2000@gmail.com>
2023-11-04 00:37:10 +0530

    dashboard ui and sidebar done, image and conversation route  work in progress

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Heading from "@/components/heading";
+
+const ImagePage = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ImagePage;
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/navbar"
+import Sidebar from "@/components/sidebar"
+import React from "react"
+
+const DashboardLayout = ({
+    children
+}:{
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="h-full relative">
+            <div className="hidden h-full md:flex md:flex-col md:fixed md:w-72 md:inset-y-0 z-[80] bg-gray-900">
+                <div>
+                    <Sidebar />
+                </div>
+            </div>
+            <main className="md:pl-72">
+                <div>
+                    <Navbar />
+                    {children}
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default DashboardLayout
